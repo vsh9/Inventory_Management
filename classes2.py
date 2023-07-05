c6864b55de537f020718b7ed1f77a15586d14c11
vsh9 <vsh9945@gmail.com>
2023-07-05 10:17:52 +0530

    comitted

--- classes2.py
+++ classes2.py
@@ -5,5 +5,13 @@ class Student:
         self.gender=gender
     def info(self):
         print(f"Name:,{self.name} | Age:,{self.age} | Gender: {self.gender}")
+
 ram=Student("Ram",18,'Male')
 ram.info()
+
+student_list=[("StudentM1",19,"Male"), ("StudentM2",20,"Female")]
+students=[]
+for name,age,gender in student_list:
+    s=Student(name,age,gender)
+    students.append(s)
+    s.info()
