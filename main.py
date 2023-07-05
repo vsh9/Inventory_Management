2827d4566411cf46890dc8ca081bcaed603673be
vsh9 <vsh9945@gmail.com>
2023-07-05 15:47:01 +0530

    comitted

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ app=typer.Typer()
 def add(name: str,price:int,quantity:int):
     item=Inventory.create(name=name,price=price,quantity=quantity)
     pass
-""""
+
+@app.command()
+def display():
+    items=Inventory.select()
+    for i in items:
+        print(f"{i.name} {i.price} {i.quantity}")
+
+
+@app.command()
+def remove(name: str):
+    item=Inventory.get(name=name)
+    item.delete_instance()
+
 @app.command()
 def goofbye(name:str,formal:bool=False):
     pass
-"""
+
     
 if __name__ == "__main__":
     create_tables()
