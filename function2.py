8e17e999d7dbf43353d9849dfd83797c53ba9188
vsh9 <vsh9945@gmail.com>
2023-07-05 15:16:28 +0530

    update classes2.py

--- function2.py
+++
@@ -1,7 +0,0 @@
-def interest(p,q,r):
-    s=(p*q*r)/100
-    print(s)
-    return s,p
-a=interest(1000,10,10)
-print(a)
-
--- function3.py
+++
@@ -1,8 +0,0 @@
-def switch(x,y):
-    x,y=y,x
-    print("inside switch:",end='')
-    print("x=",x,"y=",y)
-x=5
-y=7
-switch(x,y)
-print("the old x,y are",x,y)
